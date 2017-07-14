5ce46367af28cf813c74a62629f96a93b5bf7905
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-07-14 10:34:22 -0400

    completed basic server setup
    '

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
+
+const publicPath  = path.join(__dirname,'/public');
+app.use(express.static(publicPath));
+server.listen(port);
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
