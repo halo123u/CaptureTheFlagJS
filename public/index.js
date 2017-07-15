e6c47344d43f9087bae73e8be9080938fb2c49c2
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-07-14 23:04:01 -0400

    added player functionality

--- public/index.js
+++ public/index.js
@@ -5,31 +5,59 @@ var $container = $('<div>').addClass('container');
     $container.append($('<div>').addClass('box').attr('id', 'box1').css('background', 'blue'));
     $container.append($('<div>').addClass('box').attr('id', 'box2').css('background', 'red'));
     var box1 = document.querySelector('#box1');
-
-socket.on('connect', function(data){
+    var ID = null;
+socket.on('connect', function(){
     console.log('connected to server');
+    socket.on('setID',(data) =>{
+        console.log(data);
+        ID=data;
+    });
     $(document).ready(function(){
     socket.on('user',function(box){
         console.log(box);
-        box1.style.left= `${box.left}px`;
-        box1.style.top= `${box.top}px`;
+        if(ID==1){
+            box1.style.left= `${box.left}px`;
+            box1.style.top= `${box.top}px`;
+        } else if(ID==2){
+            box2.style.left= `${box.left}px`;
+            box2.style.top= `${box.top}px`;
+        }
     });
     socket.on('render',function(box){
-        box1.style.left= `${box.left}px`;
-        box1.style.top= `${box.top}px`;
+        if(ID==1){
+            box1.style.left= `${box.left}px`;
+            box1.style.top= `${box.top}px`;
+        } else if(ID==2){
+            box2.style.left= `${box.left}px`;
+            box2.style.top= `${box.top}px`;
+        }
     });
+    // } else if(){
+    //     socket.on('user',function(box){
+    //     console.log(box);
+    //     box2.style.left= `${box.left}px`;
+    //     box2.style.top= `${box.top}px`;
+    // });
+    // socket.on('render',function(box){
+    //     box2.style.left= `${box.left}px`;
+    //     box2.style.top= `${box.top}px`;
+    // });
+
+    // }
 
 });
 });
     window.addEventListener('keydown',function(e){
         console.log(e.keyCode)
+        // if(ID==1){
         if(e.keyCode ==39){
-            socket.emit('move', e.keyCode);
+            socket.emit('move', {key:39, id:ID});
         } else if(e.keyCode ==37){
-            socket.emit('move',e.keyCode);
+            socket.emit('move',{key:37, id:ID});
         } else if(e.keyCode==38){
-            socket.emit('move',e.keyCode);
+            socket.emit('move',{key:38, id:ID});
         } else if(e.keyCode == 40){
-            socket.emit('move',e.keyCode);
+            socket.emit('move',{key:40, id:ID});
         }
+        // }
     });
--- server.js
+++ server.js
@@ -15,36 +15,55 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-var playercount = 2;
+var playerCount = 2;
 function box(top,left,id){
     this.top=top,
     this.left=left,
     this.id=id
 }
+var player1 = new box(0,0,1);
+var player2 = new box(0,0,2);
+var players = [player1,player2]
 io.on('connection', (socket) =>{
-    console.log('client connected');
-    var player1 = new box(0,0,playercount);
-    console.log(player1.top);
-       socket.emit('user', {top:player1.top, left:player1.left, id: player1.id});
-    playercount--;
-    socket.on('move',(data)=>{
-        if(data==39){
-            player1.left+=100;
-            io.sockets.emit('render',player1);
-        } else if(data==37){
-            player1.left-=100;
-            io.sockets.emit('render',player1);
-        } else if(data==38){
-            player1.top-=100;
-            io.sockets.emit('render',player1);
-        } else if(data==40){
-            player1.top+=100;
-            io.sockets.emit('render',player1);
-        }
-    })
-    socket.on('disconnect', ()=>{
-    playercount++;
-    console.log(playercount);
-});
-
+    if(playerCount>0){
+        console.log('client connected');
+        socket.emit('setID',player1.id);
+        socket.emit('user', {top:player1.top, left:player1.left});
+        socket.on('move',(data)=>{
+            //    console.log(data.key, data.id);
+            if(data.id==1){       
+                if(data.key==39){
+                    player1.left+=100;
+                    io.sockets.emit('render',player1);
+                } else if(data.key==37){
+                    player1.left-=100;
+                    io.sockets.emit('render',player1);
+                } else if(data.key==38){
+                    player1.top-=100;
+                    io.sockets.emit('render',player1);
+                } else if(data.key==40){
+                    player1.top+=100;
+                    io.sockets.emit('render',player1);
+                }
+            } else if(data.id==2){       
+                if(data.key==39){
+                    player2.left+=100;
+                    io.sockets.emit('render',player2);
+                } else if(data.key==37){
+                    player2.left-=100;
+                    io.sockets.emit('render',player2);
+                } else if(data.key==38){
+                    player2.top-=100;
+                    io.sockets.emit('render',player2);
+                } else if(data.key==40){
+                    player2.top+=100;
+                    io.sockets.emit('render',player2);
+                }
+            }   
+        });
+        socket.on('disconnect', ()=>{
+        playerCount++;
+        console.log(playerCount);
+        });
+    }
 });
