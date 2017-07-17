5e14c5eb85135322e7ac11254da4e530ff172d97
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-07-17 02:33:57 -0400

    refactored code, added winning conditions and player 4

--- public/index.js
+++ public/index.js
@@ -4,19 +4,23 @@ var $container = $('<div>').addClass('container');
 var $scoreBoard = $('<div>').addClass('score').attr('id','score1');
 var $scoreBoard2 = $('<div>').addClass('score').attr('id','score2');
 var $scoreBoard3 = $('<div>').addClass('score').attr('id','score3');
+var $scoreBoard4 = $('<div>').addClass('score').attr('id','score4');
 var $pointBox = $('<div>').addClass('point');
     $body.append($container);
     $container.append($('<div>').addClass('box').attr('id', 'box1').css('background', 'blue'));
     $container.append($('<div>').addClass('box').attr('id', 'box2').css('background', 'red'));
     $container.append($('<div>').addClass('box').attr('id', 'box3').css('background', 'green'));
+    $container.append($('<div>').addClass('box').attr('id', 'box4').css('background', 'orange'));
     $container.append($pointBox.css('background', 'black'));
     $container.append($scoreBoard);
     $container.append($scoreBoard2);
     $container.append($scoreBoard3);
+    $container.append($scoreBoard4);
 
 var box1 = document.querySelector('#box1');
 var box2 = document.querySelector('#box2');
 var box3 = document.querySelector('#box3');
+var box4 = document.querySelector('#box4');
 var point = document.querySelector('.point');
 var ID = null;
 
@@ -39,6 +43,9 @@ socket.on('connect', function(){
             
             box3.style.left= `${players.left3}%`;
             box3.style.top= `${players.top3}%`;
+            
+            box4.style.left= `${players.left4}%`;
+            box4.style.top= `${players.top4}%`;
 
             point.style.left = `${players.pLeft}%`;
             point.style.top = `${players.pTop}%`;
@@ -46,6 +53,7 @@ socket.on('connect', function(){
             $scoreBoard.text(`${players.p1s}`);
             $scoreBoard2.text(`${players.p2s}`);
             $scoreBoard3.text(`${players.p3s}`);
+            $scoreBoard4.text(`${players.p4s}`);
      });
     socket.on('render',function(box){
          if(box.id==1){
@@ -57,17 +65,28 @@ socket.on('connect', function(){
          } else if(box.id==3){
             box3.style.left= `${box.left}%`;
             box3.style.top= `${box.top}%`;
+         } else if(box.id ==4){
+            box4.style.left= `${box.left}%`;
+            box4.style.top= `${box.top}%`;
          }
     });
-    socket.on('P',(data)=>{
+    socket.on('P',function(data){
         point.style.left= `${data.pl}%`;
         point.style.top= `${data.pt}%`;
         $scoreBoard.text(`${data.p1s}`);
         $scoreBoard2.text(`${data.p2s}`);
         $scoreBoard3.text(`${data.p3s}`);
+        $scoreBoard4.text(`${data.p4s}`)
 
 
-    }) 
+    });
+    socket.on('winner', function(color){
+        if(ID!==null){
+            if(confirm(`${color} player won!!!!`)){
+            socket.emit('reset');
+            }
+        }
+    }); 
 
 });
 });
--- server.js
+++ server.js
@@ -11,19 +11,30 @@ const publicPath  = path.join(__dirname,'/public');
 app.use(express.static(publicPath));
 server.listen(port);
 
-function box(top,left,score,id){
-    this.top=top,
-    this.left=left,
-    this.score=score,
-    this.id=id
+function Box(top,left,score,id, color){
+    this.top = top,
+    this.left = left,
+    this.score = score,
+    this.id = id,
+    this.color = color;
 }
 
+Box.prototype.getScore = function(){
+
+     if(this.score === 1){
+         io.sockets.emit('winner',this.color);
+     }
+}
+
+
 var playerCount = 0;
-var player1 = new box(0,0,0,1);
-var player2 = new box(94,96,0,2);
-var player3 = new box(0,96,0,3);
-var point = new box((Math.random()*96),(Math.random()*96), -1);
-var players = [player1,player2, player3];
+var player1 = new Box(0,0,0,1,'Blue');
+var player2 = new Box(94,96,0,2,'Red');
+var player3 = new Box(0,96,0,3, 'Green');
+var player4 = new Box(94,0,0,4,'Orange');
+var point = new Box((Math.random()*96),(Math.random()*96), -1);
+var players = [player1,player2, player3, player4];
+var reset = false;
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
@@ -44,14 +55,12 @@ var movebox = function(id,key){
                 }
                 break;
             case 38:
-                console.log('going up works');
                 if(players[id].top>0){ 
                 players[id].top -= 4.1;
                 io.sockets.emit('render',players[id]);
                 }
                 break;
             case 40:
-                console.log('goind down works');
                 if(players[id].top<94){ 
                 players[id].top += 4.1;
                 io.sockets.emit('render',players[id]);
@@ -60,56 +69,66 @@ var movebox = function(id,key){
         }
 }
 
+var collDet = (id) => {
+    if((players[id].left<= point.left) && (players[id].left+3) >= point.left
+             && (players[id].top<= point.top && (players[id].top+5) >= point.top)){
+                    players[id].score++;
+                    point.left = (Math.random()*96);
+                    point.top = (Math.random()*96);
+                    players[id].getScore();
+                    io.sockets.emit('P',{pl:point.left,pt:point.top, p1s:player1.score , p2s:player2.score, p3s:player3.score , p4s:player4.score});                
+            } 
+}
+
+
 
 io.on('connection', (socket) =>{
-    socket.emit('init', {top1:player1.top,top2:player2.top, top3:player3.top,left1:player1.left, 
-            left2:player2.left,left3:player3.left, pLeft:point.left,pTop:point.top, p1s:player1.score, p2s:player2.score, p3s:player3.score});
-    if(playerCount<3){
+    let start = ()=>{
+    if(playerCount<=0 || reset){
+    //Player1
+    player1.left=0;
+    player1.top=0;
+    player1.score=0;
+    //Player2 
+    player2.left=96;
+    player2.top=94; 
+    player2.score= 0;
+    //Player3
+    player3.left=96;
+    player3.top=0;
+    player3.score =0;
+    //Player 4
+    player4.left=0;
+    player4.top=94;
+    player4.score =0;
+
+    }
+    io.sockets.emit('init', {top1:player1.top,top2:player2.top, top3:player3.top,top4:player4.top,
+        left1:player1.left, left2:player2.left,left3:player3.left,left4:player4.left, 
+        pLeft:point.left,pTop:point.top,
+        p1s:player1.score, p2s:player2.score, p3s:player3.score, p4s:player4.score});  
+}
+    start();
+    if(playerCount<4){
         console.log('client connected');
         socket.emit('setID',players[playerCount].id);
         playerCount++;
 
         socket.on('move',(data)=>{
-            if((player1.left<= point.left) && (player1.left+3) >= point.left
-             && (player1.top<= point.top && (player1.top+5) >= point.top)){
-                    player1.score++;
-                    console.log(player1.score);
-                    point.left = (Math.random()*96);
-                    point.top = (Math.random()*96);
-
-                    io.sockets.emit('P',{pl:point.left,pt:point.top, p1s:player1.score , p2s:player2.score, p3s:player3.score});                
-            } 
-            
-            if((player2.left<= point.left) && (player2.left+3) >= point.left
-             && (player2.top<= point.top && (player2.top+5) >= point.top)){
-
-                    player2.score++;
-                    point.left = (Math.random()*96);
-                    point.top = (Math.random()*96);
-                    io.sockets.emit('P',{pl:point.left,pt:point.top, p1s:player1.score , p2s:player2.score , p3s:player3.score});              
-            } 
-            
-             if((player3.left<= point.left) && (player3.left+3) >= point.left
-             && (player3.top<= point.top && (player3.top+5) >= point.top)){
-
-                    player3.score++;
-                    point.left = (Math.random()*96);
-                    point.top = (Math.random()*96);
-                    io.sockets.emit('P',{pl:point.left,pt:point.top, p1s:player1.score , p2s:player2.score , p3s:player3.score});              
-            } 
-                movebox((data.id-1),data.key);
+            movebox((data.id-1),data.key);
+            collDet(data.id-1);
 
         });
     }
+    socket.on('reset',()=>{
+        reset=true;
+        start();
+    });
     socket.on('disconnect', ()=>{
         playerCount--;
         if(playerCount<=0){
-            player1.left=0;
-            player1.top=0;
-            player2.left=96;
-            player2.top=94; 
-            player3.left=96;
-            player3.top=0; 
+            start();
+            reset=false; 
             playerCount=0;           
         }
         console.log(playerCount);
